7026aadf5720d22b0505197bda4b410b7d0c593b
rogalicko <51017645+rogalicko@users.noreply.github.com>
2026-08-07 01:16:24 +0500

    Очистка репозитория, добавление токеновой авторизации и обновление gitignore

--- init_db.py
+++
@@ -1,9 +0,0 @@
-from database import engine
-from models import Base
-
-print("Подключаемся к PostgreSQL...")
-
-# Эта команда создает таблицы, если их еще нет
-Base.metadata.create_all(bind=engine)
-
-print("Успех! Все таблицы успешно созданы в базе colorist_crm.")
--- update_db.py
+++
@@ -1,7 +0,0 @@
-from models import Base
-# Внимание: если твой engine создается в main.py, то вместо 'database' напиши 'main'
-from database import engine
-
-print("Подключение к базе данных...")
-Base.metadata.create_all(bind=engine)
-print("Все недостающие таблицы успешно созданы!")
